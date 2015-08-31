39167710843717f21e9a24d768d636756a59005d
Tobias Abarbanell <tobias@abarbanell.de>
2015-08-31 11:36:16 +0200

    Allow MONGOURL environment variable, if set.

--- express.js
+++ express.js
@@ -8,7 +8,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(logger('dev'))
 
-var db = mongoskin.db('mongodb://@localhost:27017/test', {safe:true})
+var mongourl = process.env.MONGOURL || 'mongodb://@localhost:27017/test';
+var db = mongoskin.db(mongourl, {safe:true})
 
 app.param('collectionName', function(req, res, next, collectionName){
   req.collection = db.collection(collectionName)
